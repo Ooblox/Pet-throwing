e0f04707ce637785142e71c2eb3326df8104d1e6
JacobSteinberg81 <jacobsteinberg81@gmail.com>
2023-07-25 18:43:17 -0400

    m

--- src/shared/CreateClass.lua
+++ src/shared/CreateClass.lua
@@ -1,3 +1,16 @@
-return function()
-	print("Hello, world!")
+-- Function to create a class
+
+local CreateClass = function(setProperties)
+	local class = {}
+	class.__index = class
+	
+	setProperties(class)
+	
+	return {new = function()
+		local newObject = setmetatable({}, class)
+		
+		return newObject
+	end,}
 end
+
+return CreateClass
