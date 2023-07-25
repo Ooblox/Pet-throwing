0d2c979b7f3bd14dcd643a029760834bdcfc72d8
JacobSteinberg81 <jacobsteinberg81@gmail.com>
2023-07-25 18:59:06 -0400

    n

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,46 @@
-print("Hello world, from server!")
+local Dss = game:GetService("DataStoreService")
+local Ds = Dss:GetDataStore("Players")
+
+local CreateClass = require(game.ReplicatedStorage.Shared.CreateClass)
+
+local PlayerClass = CreateClass(function(self)
+    self.Instance = nil
+    self.SaveData = nil
+
+    self.BaseData = {
+
+    }
+
+    self.SaveData = function()
+        
+    end
+
+    self.LoadData = function()
+        
+    end
+end)
+
+local PlayersManagerClass = CreateClass(function(self)
+    self.CurrentPlayerObjects = {}
+
+    self.PlayerRemovingManager = function()
+        game.Players.PlayerRemoving:Connect(function(Player)
+            for i, v in pairs(self.CurrentPlayerObjects) do
+                if v.Instance == Player then
+                    v.SaveData()
+                    table.remove(self.CurrentPlayerObjects, table.find(self.CurrentPlayerObjects, v))
+                end
+            end
+        end)
+    end
+
+    self.PlayerJoiningManager = function()
+        game.Players.PlayerAdded:Connect(function(Player)
+            local PlayerObj = PlayerClass.new()
+            PlayerObj.Instance = Player
+            PlayerObj.LoadData()
+
+            table.insert(self.CurrentPlayerObjects, PlayerObj)    
+        end)
+    end
+end)
