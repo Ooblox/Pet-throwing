eb7b751ee77b99a3c2ab2c14253a1688e66b8a83
JacobSteinberg81 <jacobsteinberg81@gmail.com>
2023-07-31 21:29:28 -0400

    n

--- src/client/CreateDataDisplayGuis.lua
+++ src/client/CreateDataDisplayGuis.lua
@@ -6,9 +6,9 @@ local Player = game.Players.LocalPlayer
 
 return(function(self)
     self.TextLabels = {
-        [Player.PlayerGui:WaitForChild("Menu").Top.Cash.TextLabel] = "Cash",
-        [Player.PlayerGui.Menu.Top.Multiplier.TextLabel] = "Multiplier",
-        [Player.PlayerGui.Menu.Top.ThrowingPower.TextLabel] = "Strength",
+        [Player.PlayerGui:WaitForChild("MainFrames").Top.Cash.TextLabel] = "Cash",
+        [Player.PlayerGui.MainFrames.Top.Multiplier.TextLabel] = "Multiplier",
+        [Player.PlayerGui.MainFrames.Top.ThrowingPower.TextLabel] = "Strength",
 
     }
 
